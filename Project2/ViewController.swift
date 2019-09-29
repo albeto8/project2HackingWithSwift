8427a5e1c34771a3a06fd8e1e970eeb8794fd89e
Mario Alberto Barragán Espinosa <>
2019-09-28 19:43:37 -0500

    Designing your layout

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Mario Alberto Barragán Espinosa on 9/28/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
